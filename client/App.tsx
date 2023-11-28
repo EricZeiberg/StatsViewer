cf560b5624b4b340ff41c0b483f6fcd0fc29f1d1
Eric Zeiberg <ejzeiberg@gmail.com>
2023-11-28 17:22:50 -0500

    Add async dropdown and cards

--- client/App.tsx
+++ client/App.tsx
@@ -1,31 +1,53 @@
 import Select from 'react-select'
 import AsyncSelect from 'react-select/async';
-import { View, ScrollView, StyleSheet, Image } from 'react-native';
+import { View, ScrollView, StyleSheet, Image, FlatList } from 'react-native';
 import { Text, Card, Button, Icon } from '@rneui/themed';
 import { ApolloClient, InMemoryCache, ApolloProvider, gql, useQuery } from '@apollo/client';
+import { Dispatch, SetStateAction, useRef, useState } from 'react';
 
 const client = new ApolloClient({
-  uri: 'https://localhost:4000',
+  uri: 'http://localhost:4000',
   cache: new InMemoryCache(),
 });
 
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+    flex: 1,
+  }
+})
+
 
 export default function App() {
+  const [cards, addCard] = useState<number[]>([])
   return (
     <ApolloProvider client={client}>
- <View >
-     <Card containerStyle={{ marginTop: 15 }}>
-          <Card.Title>FONTS</Card.Title>
-          <Card.Divider />
-          <Text h1>
-            h1 Heading
-          </Text>
-          <MyComponent></MyComponent>
-        </Card>
-    </View>
+      <View style={styles.center}>
+      <View>
+        <SearchBox cards={cards} addCard={addCard}></SearchBox>
+      </View>
+      <View>
+        <FlatList numColumns={4} data={cards} renderItem={({ item }) => <PlayerCard data={item} />} ></FlatList>
+      </View>
+      </View>
+     
+
     </ApolloProvider>
-   
+
   );
+}
+
+
+const PlayerCard = ({ data }: { data: number }) => {
+  return (
+    <Card key={data} containerStyle={{ marginTop: 15 }}>
+      <Card.Title>{data}</Card.Title>
+      <Card.Divider />
+      <Text h1>
+        h1 Heading
+      </Text>
+    </Card>
+  )
 }
 
 const SEARCH_PLAYERS = gql`
@@ -33,18 +55,35 @@ query FindPlayer($search: String) {
   findPlayer(search: $search) {
     firstname
     lastname
+    id
   }
 }
 `;
 
-function SearchPlayers(search: String) {
-  const { loading, error, data } = useQuery(SEARCH_PLAYERS, {
-    variables: {search}
-  });
-  return data
+const SearchPlayers = async (inputValue: String) => {
+  const res = await client.query({
+    query: SEARCH_PLAYERS, variables: {
+      search: inputValue
+    }
+  })
+  var response = res.data.findPlayer.map((a: { firstname: string; lastname: string, id: number }) => ({
+    label: a.firstname + " " + a.lastname,
+    value: a.id
+  }))
+
+  return response
 }
 
 
-const MyComponent = () => (
-  <Select options={options} />
-)
+const SearchBox = ({ cards, addCard }: { cards: number[], addCard: Dispatch<SetStateAction<number[]>> }) => {
+  const selectedInputRef = useRef<any>();
+
+  return ( <AsyncSelect isClearable ref={selectedInputRef} onChange={(option: { label: string, value: string } | null) => {
+    if (option != null) {
+      addCard([...cards, parseInt(option.value)])
+    }
+  }} loadOptions={SearchPlayers} />)
+ 
+}
+
+
